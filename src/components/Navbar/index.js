3785024a3c8b893ca6db2f930a5e2e3b2b6f6ab3
KrishnaDass95 <krishdass95@gmail.com>
2023-10-15 16:05:35 +0530

    Make navbar responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,14 @@
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen) // toggling the state
+    }
 
   return (
     <div className="input-wrapper">
@@ -11,12 +18,14 @@ const Navbar = () => {
             SPHYRNA
           </NavLink>
         </div>
-        <NavLink className="nav-link toggle-button" to="/">
+        <NavLink className="nav-link toggle-button" to="/" onClick={toggleMenu}>
             <span className="bar"></span>
             <span className="bar"></span>
             <span className="bar"></span>
+        
         </NavLink>
-        <div className="navbar-items-right">
+        <div className={`navbar-items-right ${menuOpen ? 'active' : ''}`}>
+        {/* <div className="navbar-items-right"> */}
           <ul>
             <li>
               <NavLink className="nav-link nav-item" to="/about">
