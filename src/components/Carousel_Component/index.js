60d55bbc390be7e3390b07d724ab1d1feffc73d9
KrishnaDass95 <krishdass95@gmail.com>
2023-10-22 16:35:46 +0530

    Get info to display on services section

--- src/components/Carousel_Component/index.js
+++ src/components/Carousel_Component/index.js
@@ -4,26 +4,26 @@ const Carousel = () => {
   const items = [
     {
       title: "Software Support",
-      img: "src",
+      src: require("../../media/software.svg"),
       desc: "We understand your product's software systems thoroughly be involved with the product development to have a good grasp on the current state to assist customers",
     },
     {
       title: "Product Insight",
-      img: "src",
+      src: require("../../media/insight.svg"),
       desc: "All the product insight required, from hardware peformances, software bugs, metrics are all collected by us to give feedback",
     },
     {
         title: "Hardware Support",
-        img: "src",
+        src: require("../../media/hardware.svg"),
         desc: "Our hardware specialists understand the hardware systems better to diagnose and provide solutions to your customers"
     }
   ];
 
   return (
-    <div class="input-wrapper">
+    <div class="input-wrapper carousel">
         {items.map((item) => {
             return (
-                <CarouselItem title={item.title} img={item.img} desc={item.desc}/>
+                <CarouselItem title={item.title} src={item.src} desc={item.desc}/>
             )
         })}
     </div>
--- src/components/Carousel_Item/index.js
+++ src/components/Carousel_Item/index.js
@@ -1,10 +1,13 @@
 
 
-export const CarouselItem = () => {
+export const CarouselItem = ({title, src, desc }) => {
 
     return (
-        <div>
-            CarouselItem
+        <div className="carousel-item">
+            <div className="header-text">{title}</div>
+            <img src={src.default} alt={desc} />
+            <div className="para-text">{desc}</div>
+            
         </div>
     )
 }
