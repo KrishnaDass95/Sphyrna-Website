195d9e7dbff9bed44e5ba54c4e27548f64cfb12a
KrishnaDass95 <krishdass95@gmail.com>
2024-03-05 20:16:56 +0530

    Add images

--- src/components/About_Component/index.js
+++ src/components/About_Component/index.js
@@ -1,5 +1,17 @@
 import "./about.css";
 import { TeamMemberCardComponent } from "../Team_Member_Card_Component";
+import Ali from "../../media/team-members/1.Ali.png"
+import Frank from "../../media/team-members/2.Frank.png"
+import Shruti from "../../media/team-members/3.Shruti.png"
+import Abhi from "../../media/team-members/7.Abhi.png"
+import Akhil from "../../media/team-members/8.Akhil.png"
+import Hari from "../../media/team-members/6.Hari.png"
+import Vishnu from "../../media/team-members/10.Vishnu.png"
+import Bijit from "../../media/team-members/9.Bijit.png"
+import Ashish from "../../media/team-members/5.Ashish.png"
+import Krishna from "../../media/team-members/4.Krishna.png"
+
+
 
 export const AboutComponent = () => {
   const team_obj = [
@@ -7,61 +19,61 @@ export const AboutComponent = () => {
       personName: "Mahoob Ali Khan",
       personTitle: "Chief Executive Officer",
       personDomain: null,
-      imagePath: "../../../media/team-members/1.Ali.png"
+      imagePath: Ali
     },
     {
       personName: "Franklin Prince",
       personTitle: "Chief Opearating Officer",
       personDomain: null,
-      imagePath: "../../../media/team-members/2.Frank.png"
+      imagePath: Frank
     },
     {
       personName: "Shruti Badhani",
       personTitle: "General Manager",
       personDomain: null,
-      imagePath: "../../../media/team-members/3.Shruti.png"
+      imagePath: Shruti
     },
     {
       personName: "Ashish Kuttapa",
       personTitle: "Manager",
       personDomain: "Rider Experience",
-      imagePath: "../../../media/team-members/5.Ashish.png"
+      imagePath: Ashish
     },
     {
       personName: "Hari Pulikode",
       personTitle: "Lead Product Expert",
       personDomain: "Sotware",
-      imagePath: "../../../media/team-members/6.Hari.png"
+      imagePath: Hari
     },
     {
       personName: "Abhishek Anand",
       personTitle: "Lead Product Expert",
       personDomain: "Hardware",
-      imagePath: "../../../media/team-members/7.Abhi.png"
+      imagePath: Abhi
     },
     {
       personName: "Akhil Khalibhat",
       personTitle: "Managar",
       personDomain: "Rider Experience",
-      imagePath: "../../../media/team-members/8.Akhil.png"
+      imagePath: Akhil
     },
     {
       personName: "Bijit Sinha",
       personTitle: "Senior Engineer",
       personDomain: "Rider Experience",
-      imagePath: "../../../media/team-members/9.Bijit.png"
+      imagePath: Bijit
     },
     {
       personName: "Vishnu Raghunandhan",
       personTitle: "Engineer",
       personDomain: "Rider Experience",
-      imagePath: "../../../media/team-members/10.Vishnu.png"
+      imagePath: Vishnu
     },
     {
       personName: "Krishna Dass",
       personTitle: "Senior Engineer",
       personDomain: "Software",
-      imagePath: "../../../media/team-members/4.Krishna.png"
+      imagePath: Krishna
     }
   ];
   return (
